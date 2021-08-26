817eb41083d175524fa7a19cb9405b5c82c066a5
Thuan Bui <thuanbui87@gmail.com>
2021-08-26 22:18:40 +0700

    re-add public folder

--- part1/1.12-1.14-anecdotes/src/App.js
+++ part1/1.12-1.14-anecdotes/src/App.js
@@ -14,26 +14,46 @@ const App = () => {
   const [selected, setSelected] = useState(0)
   const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
   
+  console.log(points)
+
   const setRandom = () => {
     const min = 0
     const max = anecdotes.length-1
     const random = Math.floor(Math.random() * (max-min+1)) + min
-    console.log(random)
+    //console.log(random)
     setSelected(random)
   }
 
   const setVote = () => {
-    const newpoints = {...points}
+    const newpoints = [...points]
     newpoints[selected] += 1
     setPoints(newpoints)
-    console.log(points)
   }
 
+  const Best = (props) => {
+    const best = props.data
+    const mostvote = Math.max(...best)
+    const index = best.indexOf(mostvote);
+    return (
+      <div>
+      <h2>Anecdote with most votes</h2>
+        <p>{props.notes[index]}</p>
+        <span>has {mostvote} votes</span>
+      </div>
+    )
+  }
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <p>has {points[selected]} votes</p>
-      <button onClick={setVote}>vote</button><button onClick={setRandom}>Next Anecdote</button>
+      <div>
+        <h1>Anecdote of the day</h1>
+        <p>{anecdotes[selected]}</p>
+        <p>has {points[selected]} votes</p>
+        <button onClick={setVote}>vote</button><button onClick={setRandom}>Next Anecdote</button>
+      </div>
+      
+        <Best data={points} notes={anecdotes}/>
+
+      
     </div>
   )
 }
